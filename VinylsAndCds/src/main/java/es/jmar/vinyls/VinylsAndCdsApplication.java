b3ba6081708b4dade3a8ef7a4e3558e43b63c105
DESKTOP-D04QR97\Jesus <jesusmgit97@gmailcom>
2022-05-23 09:12:51 +0200

    Commit inicial

---
+++ VinylsAndCds/src/main/java/es/jmar/vinyls/VinylsAndCdsApplication.java
@@ -0,0 +1,13 @@
+package es.jmar.vinyls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylsAndCdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylsAndCdsApplication.class, args);
+	}
+
+}
